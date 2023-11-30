b3aa966a6fdb376ef71be8d71739ed1c3ac51d7a
nouhaz01 <151826196+nouhaz01@users.noreply.github.com>
2023-11-30 15:02:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,53 +7,27 @@ import matplotlib.pyplot as plt
 
 
 def dim_red(mat, p, method):
-    '''
-    Perform dimensionality reduction
-
-    Input:
-    -----
-        mat : NxM list 
-        p : number of dimensions to keep 
-    Output:
-    ------
-        red_mat : NxP list such that p<<m
-    '''
     if method=='ACP':
         red_mat = mat[:,:p]
-        
     elif method=='AFC':
         red_mat = mat[:,:p]
-        
     elif method=='UMAP':
-        red_mat = mat[:,:p]
-        
+        umap_model = UMAP(n_components=p)
+        red_mat = umap_model.fit_transform(mat)    
     else:
         raise Exception("Please select one of the three methods : APC, AFC, UMAP")
-    
     return red_mat
 
 
 def clust(mat, k):
-    '''
-    Perform clustering
-
-    Input:
-    -----
-        mat : input list 
-        k : number of cluster
-    Output:
-    ------
-        pred : list of predicted labels
-    '''
-    
-    pred = np.random.randint(k, size=len(mat))
-    
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    pred = kmeans.fit_predict(mat)
     return pred
 
 # import data
-ng20 = fetch_20newsgroups(subset='test')
-corpus = ng20.data[:2000]
-labels = ng20.target[:2000]
+data = fetch_20newsgroups(subset='all', remove=('headers', 'footers', 'quotes'))
+corpus = data.data[:2000]
+labels = data.target[:2000]
 k = len(set(labels))
 
 # embedding
